1f47f1f86e390bb888e8bc218c410fd4a03ffc73
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-11-01 17:50:04 -0400

    Add Lesson_17_Integración_Con_Cypress_Para_Entornos_Complejos/part_5_pruebas_criticas.js

---
+++ Lesson_17_Integración_Con_Cypress_Para_Entornos_Complejos/part_5_pruebas_criticas.js
@@ -0,0 +1,12 @@
+describe('Suite de Pruebas Críticas', () => {
+    it('Prueba 1', () => {
+        // implementación de la prueba
+    });
+
+    it('Prueba 2', () => {
+        // implementación de la prueba
+    });
+});
+
+// Para ejecutar solo rápidamente las pruebas críticas, puedes hacerlo desde la línea de comandos:
+npx cypress run --spec 'cypress/integration/critical/**/*.js'
